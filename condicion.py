c14cd86f752a907ffc0f718a28c31c814fc480aa
franciscomelov <femva3012@hotmail.com>
2020-10-21 20:42:47 -0500

    condicional

---
+++ condicion.py
@@ -0,0 +1,27 @@
+nombre = input("Ingresa tu nombre: ")
+
+saludo = "hola "
+print(saludo + nombre)
+
+edad = input("Cual es tu edad: ")
+
+mayoria_de_edad = 18
+
+edad = int(edad)
+
+#   17   >=  18
+if edad >= mayoria_de_edad:
+    print("Eres MAYOR de edad")
+else:
+    print("Eres MENOR de edad")
+
+'''
+if condicional:
+    que hacer si se cumple
+else:
+    si no se cumple
+    
+'''
+
+
+
